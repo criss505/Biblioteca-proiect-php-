cc34fbb1cbf1047a29c888812f4c3ab030ab894c
criss505 <100491898+criss505@users.noreply.github.com>
2022-11-02 21:25:34 +0200

    Add files via upload

---
+++ bib++/index.php
@@ -0,0 +1,40 @@
+<html>
+
+<head>
+    <title> bib++ </title>
+
+</head>
+
+<body>
+    <h2> bib++ </h2>
+
+    <?php
+        echo "Pagina sub constructie"
+    ?>
+    <p> Bine ati venit! Aici voi crea in viitorul apropiat proiectul meu (Damov Cristina,252):
+         <b> O biblioteca </b> </p>
+
+    <p> Idea proiectului va fi implementarea unei biblioteci a facultatii, unde studentii isi pot
+        crea un cont (admis si de bibliotecar) si pot imprumuta/returna cartile dorite. In cazul
+        in care o carte nu este inapoiata la termenul stabilit, pot aparea penalizari. De asemenea,
+        un student poate comunica cu bibliotecarul prin mesaje.
+    </p>
+
+    <img src="./erd.png">
+
+    <ul> posibile idei de implementat:
+        <li> autentificare student </li>
+        <li> editare profil student </li>
+        <li> recuperare parola student </li>
+        <li> cautare carte </li>
+        <li> filtrare/sortare carti </li>
+        <li> rezervare/returnare carte </li>
+        <li> validare cont student de catre admin </li>
+        <li> penalizarea cartilor returnate tarziu (sau deloc :)) </li>
+        <li> comunicarea student-admin </li>
+        <li> notificari prin email/sms </li>
+        <li> eventual sistem de plati online </li>
+        <li> pagina stiri/informatii </li>
+    </ul>
+
+</body>
